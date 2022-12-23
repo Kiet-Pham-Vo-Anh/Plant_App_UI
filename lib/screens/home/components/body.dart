bd076ec4c6a5ac3a195622d8e05c27bdc14aa2c2
KietPham <kietpva0102@gmail.com>
2022-12-23 17:34:59 +0700

    Title with more button

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/constants.dart';
-
+import 'package:flutter/material.dart';
 import 'header_with_searchbox.dart';
+import 'title_with_more_button.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -11,7 +12,13 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Column(
-      children: <Widget>[HeaderSearchBox(size: size)],
+      children: <Widget>[
+        HeaderSearchBox(size: size),
+        TitleWithMoreButton(
+          title: "Recommended",
+          press: () {},
+        ),
+      ],
     );
   }
 }
