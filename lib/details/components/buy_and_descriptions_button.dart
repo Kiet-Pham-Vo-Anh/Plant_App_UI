c03db0345ea129c17714512334762c430ab9e7dc
KietPham <kietpva0102@gmail.com>
2022-12-29 17:09:40 +0700

    detached components button

--- lib/details/components/buy_and_descriptions_button.dart
+++ lib/details/components/buy_and_descriptions_button.dart
@@ -11,42 +11,56 @@ class BuyAndDescriptionsButton extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Row(
-      children: <Widget>[
-        SizedBox(
-          width: size.width / 2,
-          height: 84,
-          child: TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              backgroundColor: kPrimaryColor,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(topRight: Radius.circular(20)),
-              ),
-            ),
-            child: const Text(
-              "Buy Now",
-              style: TextStyle(color: Colors.white, fontSize: 16),
-            ),
-          ),
+      children: const <Widget>[
+        BuyAndDescriptionBtn(
+          title: 'Buy Now',
+          backgroundColor: kPrimaryColor,
+          textColor: Colors.white,
+          borderRadiusGeometry:
+              BorderRadius.only(topRight: Radius.circular(20)),
         ),
-        SizedBox(
-          width: size.width / 2,
-          height: 84,
-          child: TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              // backgroundColor: kPrimaryColor,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(20)),
-              ),
-            ),
-            child: const Text(
-              "Descriptions",
-              style: TextStyle(color: Colors.black, fontSize: 16),
-            ),
-          ),
-        ),
+        BuyAndDescriptionBtn(
+          borderRadiusGeometry: BorderRadius.only(topLeft: Radius.circular(20)),
+          title: "Description",
+          textColor: kTextColor,
+        )
       ],
+    );
+  }
+}
+
+class BuyAndDescriptionBtn extends StatelessWidget {
+  const BuyAndDescriptionBtn({
+    Key? key,
+    this.backgroundColor,
+    this.textColor,
+    required this.borderRadiusGeometry,
+    required this.title,
+  }) : super(key: key);
+
+  final Color? backgroundColor, textColor;
+  final BorderRadiusGeometry borderRadiusGeometry;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width / 2,
+      height: 84,
+      child: TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: borderRadiusGeometry,
+          ),
+        ),
+        child: Text(
+          title,
+          style: TextStyle(color: textColor, fontSize: 16),
+        ),
+      ),
     );
   }
 }
